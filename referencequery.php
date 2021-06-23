d9583920aaf89b71bf75d4078f01685d27ef5954
nipson <nipsan007@gmail.com>
2021-06-23 22:40:38 +0545

    Added oci in database library

--- referencequery.php
+++ referencequery.php
@@ -1,11 +1,11 @@
 <?php
 include 'utils/database.php';
 
-// query("INSERT INTO Users VALUES (3, '343234', 'GLOBAL BANK', '90123456', 'Password document');");
+// query("INSERT INTO Users VALUES (2, '123456', 'NABIL BANK', '12345678', 'Citizenships document')");
 
 
-// $rows = fetch_all_row("SELECT * FROM Users");
-// print_r($rows);
+$rows = fetch_all_row("SELECT * FROM Users");
+print_r($rows);
 
 
 // $row = fetch_row("SELECT * FROM Users");
--- utils/database.php
+++ utils/database.php
@@ -1,4 +1,43 @@
 <?php
+
+    /////////////////////////////////////////
+    /////////////////OCI////////////////////
+    ////////////////////////////////////////
+
+    // $conn = oci_connect('admin', 'Password!123', '//localhost/xe');
+    // if (!$conn) {
+    //     $m = oci_error();
+    //     echo $m['message'], "\n";
+    //     exit;
+    // }
+
+
+    // function query($sql){
+    //     global $conn;
+    //     $stmt = oci_parse($conn, $sql);
+    //     oci_execute($stmt);
+    // }
+
+    // function fetch_all_row($sql){
+    //     global $conn;
+    //     $stmt = oci_parse($conn, $sql);
+    //     oci_execute($stmt);
+    //     oci_fetch_all($stmt, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+    //     return $res;
+    // }
+
+    // function fetch_row($sql){
+    //     global $conn;
+    //     $stmt = oci_parse($conn, $sql);
+    //     oci_execute($stmt);
+    //     $res = oci_fetch_assoc($stmt);
+    //     return $res;
+    // }
+    
+    /////////////////////////////////////////
+    /////////////////PDO////////////////////
+    ////////////////////////////////////////
+
     try {
         $conn = new PDO("sqlite:database.sqlite");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -15,14 +54,15 @@
     function fetch_all_row($sql){
         global $conn;
         $stmt = $conn->prepare($sql);
-        $res = $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
     }
 
     function fetch_row($sql){
         global $conn;
         $stmt = $conn->prepare($sql);
         $res = $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        $res = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $res;
     }
-?>
