523b3cffb887304f37502d8ad9a4f93be2110446
asrma7 <asrma.sharma@gmail.com>
2021-07-11 00:35:56 +0545

    Database updated

--- utils/database.php
+++ utils/database.php
@@ -4,80 +4,80 @@
 ///////////////////OCI///////////////////
 ////////////////////////////////////////
 
-// $conn = oci_connect('admin', 'Password!123', '//localhost/xe');
-// if (!$conn) {
-//     $m = oci_error();
-//     echo $m['message'], "\n";
-//     exit;
-// }
+$conn = oci_connect('admin', 'Password!123', '//localhost/xe');
+if (!$conn) {
+    $m = oci_error();
+    echo $m['message'], "\n";
+    exit;
+}
 
 
-// function query($sql){
-//     global $conn;
-//     $stmt = oci_parse($conn, $sql);
-//     $result= oci_execute($stmt);
+function query($sql){
+    global $conn;
+    $stmt = oci_parse($conn, $sql);
+    $result= oci_execute($stmt);
 
-//     if(!$result){
-//         die(oci_error($stmt)['message'].":".$sql);   
-//     }
+    if(!$result){
+        die(oci_error($stmt)['message'].":".$sql);   
+    }
 
-//     return true;
-// }
+    return true;
+}
 
-// function fetch_all_row($sql){
-//     global $conn;
-//     $stmt = oci_parse($conn, $sql);
-//     oci_execute($stmt);
-//     oci_fetch_all($stmt, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+function fetch_all_row($sql){
+    global $conn;
+    $stmt = oci_parse($conn, $sql);
+    oci_execute($stmt);
+    oci_fetch_all($stmt, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
 
-//     if(!empty(oci_error($stmt)['message'])){
-//         die(oci_error($stmt)['message'].":".$sql);   
-//     }
+    if(!empty(oci_error($stmt)['message'])){
+        die(oci_error($stmt)['message'].":".$sql);   
+    }
 
-//     return $res;
-// }
+    return $res;
+}
 
-// function fetch_row($sql){
-//     global $conn;
-//     $stmt = oci_parse($conn, $sql);
-//     oci_execute($stmt);
-//     $res = oci_fetch_assoc($stmt);
+function fetch_row($sql){
+    global $conn;
+    $stmt = oci_parse($conn, $sql);
+    oci_execute($stmt);
+    $res = oci_fetch_assoc($stmt);
 
-//     if(!empty(oci_error($stmt)['message'])){
-//         die(oci_error($stmt)['message'].":".$sql);   
-//     }
+    if(!empty(oci_error($stmt)['message'])){
+        die(oci_error($stmt)['message'].":".$sql);   
+    }
 
-//     return $res;
-// }
+    return $res;
+}
 
-// function get_last_id($table) {
-//     global $conn;
-//     $stmt = oci_parse($conn, "SELECT ".$table."_seq.currval AS ID"." FROM DUAL;");
-//     oci_execute($stmt);
-//     $res = oci_fetch_assoc($stmt);
+function get_last_id($table) {
+    global $conn;
+    $stmt = oci_parse($conn, "SELECT ".$table."_seq.currval AS ID"." FROM DUAL;");
+    oci_execute($stmt);
+    $res = oci_fetch_assoc($stmt);
 
-//     return $res['ID'];
-// }
+    return $res['ID'];
+}
 
-// function toDate($date, $format) {
-//     return "TO_DATE('$date','$format')";
-// }
+function toDate($date, $format) {
+    return "TO_DATE('$date','$format')";
+}
 
-// function toTime($time)
-// {
-//     return "TO_DATE('$time', 'yyyy/mm/dd hh24:mi:ss')";
-// }
+function toTime($time)
+{
+    return "TO_DATE('$time', 'yyyy/mm/dd hh24:mi:ss')";
+}
 
-// function random_order()
-// {
-//     return "dbms_random.value";
-// }    
+function random_order()
+{
+    return "dbms_random.value";
+}    
 
-// function limit_result($row_count)
-// {
-//   // return "FETCH FIRST $row_count ROWS ONLY";
-//   return "";
-// }    
+function limit_result($row_count)
+{
+  // return "FETCH FIRST $row_count ROWS ONLY";
+  return "";
+}    
 
 
 /////////////////////////////////////////
@@ -147,66 +147,66 @@
 /////////////////MYSQLi/////////////////
 ////////////////////////////////////////
 
-$conn = new mysqli('remotemysql.com:3306', 'IAOYrmug9w', 'CH626V1Q8h', 'IAOYrmug9w');
+// $conn = new mysqli('remotemysql.com:3306', 'IAOYrmug9w', 'CH626V1Q8h', 'IAOYrmug9w');
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
-function query($sql)
-{
-    global $conn;
-    $result = mysqli_query($conn, $sql);
-    if (!$result) {
-        die(mysqli_error($conn) . " : SQL : " . $sql);
-    }
-    return $result;
-}
+// function query($sql)
+// {
+//     global $conn;
+//     $result = mysqli_query($conn, $sql);
+//     if (!$result) {
+//         die(mysqli_error($conn) . " : SQL : " . $sql);
+//     }
+//     return $result;
+// }
 
-function fetch_all_row($sql)
-{
-    global $conn;
-    $result = mysqli_query($conn, $sql);
-    if (!$result) {
-        die(mysqli_error($conn) . " : SQL : " . $sql);
-    }
-    $res = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    return $res;
-}
+// function fetch_all_row($sql)
+// {
+//     global $conn;
+//     $result = mysqli_query($conn, $sql);
+//     if (!$result) {
+//         die(mysqli_error($conn) . " : SQL : " . $sql);
+//     }
+//     $res = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//     return $res;
+// }
 
-function fetch_row($sql)
-{
-    global $conn;
-    $result = mysqli_query($conn, $sql);
-    if (!$result) {
-        die(mysqli_error($conn) . " : SQL : " . $sql);
-    }
-    $res = mysqli_fetch_assoc($result);
-    return $res;
-}
+// function fetch_row($sql)
+// {
+//     global $conn;
+//     $result = mysqli_query($conn, $sql);
+//     if (!$result) {
+//         die(mysqli_error($conn) . " : SQL : " . $sql);
+//     }
+//     $res = mysqli_fetch_assoc($result);
+//     return $res;
+// }
 
-function get_last_id($table) {
-    global $conn;
-    return mysqli_insert_id($conn);
-}
+// function get_last_id($table) {
+//     global $conn;
+//     return mysqli_insert_id($conn);
+// }
 
-function toDate($date, $format)
-{
-    return "STR_TO_DATE('$date', '%Y-%m-%d')";
-}
+// function toDate($date, $format)
+// {
+//     return "STR_TO_DATE('$date', '%Y-%m-%d')";
+// }
 
-function toTime($time)
-{
-    return "STR_TO_DATE('$time', '%Y/%m/%d %H:%i:%s')";
-}
+// function toTime($time)
+// {
+//     return "STR_TO_DATE('$time', '%Y/%m/%d %H:%i:%s')";
+// }
 
-function random_order()
-{
-    return "RAND()";
-}
+// function random_order()
+// {
+//     return "RAND()";
+// }
 
-function limit_result($row_count)
-{
-    return "LIMIT $row_count";
-}
+// function limit_result($row_count)
+// {
+//     return "LIMIT $row_count";
+// }
