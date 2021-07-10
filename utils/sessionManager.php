23e7834ed1905a071be567ec6d7f520abab8534f
asrma7 <asrma.sharma@gmail.com>
2021-07-10 21:29:15 +0545

    SQL optimized

--- utils/sessionManager.php
+++ utils/sessionManager.php
@@ -1,5 +1,5 @@
 <?php
-session_set_cookie_params(['samesite' => 'None', 'secure'=> true, 'httponly'=>true]);
-// session_set_cookie_params(3600, '/; samesite=None', $_SERVER['HTTP_HOST'], true, true);
+// session_set_cookie_params(['samesite' => 'None', 'secure'=> true, 'httponly'=>true]);
+session_set_cookie_params(3600, '/; samesite=None', $_SERVER['HTTP_HOST'], true, true);
 session_start();
 ?>
