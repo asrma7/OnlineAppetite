26b0e65ef884c5ed3fa65ef6e6233bbbda4aa1c8
asrma7 <asrma.sharma@gmail.com>
2021-07-18 21:06:15 +0545

    error pages bug fixed

--- 401.php
+++ 401.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/error-page.css">
 
-    <title>Page not Found</title>
+    <title>Not Authorized</title>
 </head>
 
 <body>
